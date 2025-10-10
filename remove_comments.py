dad8de65891ff8f4dcd51aecfc406aa72cccc31c
Marco Millq <marcomillq@gmail.com>
2025-10-11 00:18:37 +0200

    .

--- remove_comments.py
+++
@@ -1,7 +0,0 @@
-with open('ticket.py', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-
-with open('ticket.py', 'w', encoding='utf-8') as f:
-    for line in lines:
-        if not line.strip().startswith('#'):
-            f.write(line)
